419d0774ccf9918460c09561aafdbb2de60d5b91
Angelo <herman256561@gmail.com>
2023-12-30 20:50:59 -0500

    basic part finished

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import { weather_api_key } from "./config.js";
 
-const weather_api_url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=pittsburgh"
+const weather_api_url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+const errorMessage = document.querySelector(".error");
 
-async function checkweather(){
-    const data = await fetch(weather_api_url + `&appid=${weather_api_key}`)
+async function checkweather(city){
+    const data = await fetch(weather_api_url + city + `&appid=${weather_api_key}`)
     .then((response)=>response.json())
     .then((result)=>{
         return result;
     });
+
+    if(data.cod == 404){
+        errorMessage.style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
+        errorMessage.style.display = "none";
+        document.querySelector(".weather").style.display = "block";
+    }
 
     console.log(data);
 
@@ -15,7 +27,23 @@ async function checkweather(){
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+    }else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+    }else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";
+    }else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    }else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
 }
 
-checkweather();
+searchBtn.addEventListener("click", ()=>{
+    checkweather(searchBox.value);
+});
+
+checkweather("pittsburgh");
 
